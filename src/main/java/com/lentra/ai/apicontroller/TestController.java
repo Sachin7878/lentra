27bc035fb30773adb204bb45faec531faf475d83
Sachin Sheelavant <sheelavantsachin@gmail.com>
2024-01-22 18:13:04 +0530

    Improvements
    - added logger
    - changed mysql pv storage class
    - added grafana nodeport

--- src/main/java/com/lentra/ai/apicontroller/TestController.java
+++ src/main/java/com/lentra/ai/apicontroller/TestController.java
@@ -1,5 +1,7 @@
 package com.lentra.ai.apicontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,23 +15,28 @@ import com.lentra.ai.repository.UserRepository;
 @RestController
 public class TestController {
 	
+	private static Logger log = LoggerFactory.getLogger(TestController.class);
+	
 	@Autowired
 	UserRepository userRepo;
 	
 	@GetMapping("/test")
 	  String testApi() {
+		log.info("Invoking test API!");
 	    return "Hello";
 	}
 	
 //	@GetMapping("/users")
 //    public ResponseEntity<List<UserModel>> getAllUsers(){
-//        List<UserModel> users = userRepo.findAll();
+//		log.info("Fetching all users");
+//		List<UserModel> users = userRepo.findAll();
 //        return new ResponseEntity<>(users, HttpStatus.OK);
 //    }
 	
 	@PostMapping("/users")
 	public ResponseEntity<?> createUser(
 			@RequestBody UserModel user) {
+		log.info("Creating user with details: {}", user);
 		return ResponseEntity.ok(userRepo.save(user));
 	}
 }
